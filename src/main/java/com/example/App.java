75fc7676f82c50fcd5c86b7dbfe5c37824659906
csharp1994 <csharp1994@gmail.com>
2022-12-01 16:08:22 -0500

    Initial Commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,61 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static int start;
+    private static int end;
+    private static List<String> nums;
+    private static HashMap<String, Integer> results;
+
+    public static void main( String[] args ) {
+
+        if (args.length != 3) {
+            System.out.println("Input Error: Must supply a starting number, an ending number, and one or more comma delimited digits to look up.");
+            System.exit(1);
+        }
+
+        try {
+            start = Integer.parseInt(args[0]);
+            end = Integer.parseInt(args[1]);
+
+            String[] inputNums = args[2].split(",");
+            nums = Arrays.asList(inputNums);
+            results = new HashMap<>();
+            for (String num: nums) {
+                results.put(num, 0);
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Input Error: Inputs must be numbers."); 
+            System.exit(1);
+            // test
+        }
+
+        
+
+        for (int i = start; i <= end; i++) {
+            char[] digitArr = Integer.toString(i).toCharArray();
+            for (char digit : digitArr) {
+                String digitStr = Character.toString(digit);
+                if (nums.contains(digitStr)) {
+                    results.put(digitStr, results.get(digitStr) + 1);
+                }
+            }
+        }
+
+
+        for (Map.Entry<String, Integer> result : results.entrySet()) {
+            System.out.println(result.getKey() + " occurs " + result.getValue() + " times.");
+        }
+    }
+}
